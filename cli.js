f3b2dd47f869672bf13698ecb44e5072f1de623f
jelimarl <jelimarlm@gmail.com>
2022-11-01 11:35:59 -0500

    Se agregó color a los mensajes y resultados que salen en consola

--- cli.js
+++ cli.js
@@ -10,39 +10,39 @@ const args = process.argv
 function cli(route, args) {
   if (args.length === 3) {
     mdLinks(route, { validate: false }).then((value) => {
-      if (value === 'There are no markdown files' || value === 'There are no links' || value === 'That path does not exist') { console.log(value) }
-      else { value.forEach((element) => console.log(element.file, element.href, element.text)) }
+      if (value === 'There are no markdown files' || value === 'There are no links' || value === 'That path does not exist') { console.log(chalk.bgRed(value)) }
+      else { value.forEach((element) => console.log(element.file, element.href, chalk.greenBright(element.text))) }
     })
   }
 
   else if (args.length === 4 && args[3] === '--validate') {
     mdLinks(route, { validate: true }).then((value) => {
-      if (value === 'There are no markdown files' || value === 'There are no links' || value === 'That path does not exist') { console.log(value) }
-      else { value.forEach((element) => console.log(element.file, element.href, element.ok, element.status, element.text)) }
+      if (value === 'There are no markdown files' || value === 'There are no links' || value === 'That path does not exist') { console.log(chalk.bgRed(value)) }
+      else { value.forEach((element) => console.log(element.file, element.href, chalk.greenBright(element.ok), element.status, chalk.greenBright(element.text))) }
     })
   }
 
   else if (args.length === 4 && args[3] === '--stats') {
     mdLinks(route, { validate: false }).then((value) => {
-      if (value === 'There are no markdown files' || value === 'There are no links' || value === 'That path does not exist') { console.log(value) }
+      if (value === 'There are no markdown files' || value === 'There are no links' || value === 'That path does not exist') { console.log(chalk.bgRed(value)) }
       else {
         const stats = statsLinks(value)
-        console.log(`Total: ${stats.Total}\nUnique: ${stats.Unique}`)
+        console.log(`Total: ${chalk.greenBright(stats.Total)}\nUnique: ${chalk.greenBright(stats.Unique)}`)
       }
     })
   }
 
   else if (args.length === 5 && args.includes('--stats') && args.includes('--validate')) {
     mdLinks(route, { validate: true }).then((value) => {
-      if (value === 'There are no markdown files' || value === 'There are no links' || value === 'That path does not exist') { console.log(value) }
+      if (value === 'There are no markdown files' || value === 'There are no links' || value === 'That path does not exist') { console.log(chalk.bgRed(value)) }
       else {
         const stats = statsLinks(value)
-        console.log(`Total: ${stats.Total}\nUnique: ${stats.Unique}\nBroquen: ${stats.Broken}`)
+        console.log(`Total: ${chalk.greenBright(stats.Total)}\nUnique: ${chalk.greenBright(stats.Unique)}\nBroquen: ${chalk.greenBright(stats.Broken)}`)
       }
     })
   }
 
-  else { console.log(chalk.redBright('Write valid options after your path')) }
+  else { console.log(chalk.bgRed('Write valid options after your path')) }
 }
 
 cli(route, args)
